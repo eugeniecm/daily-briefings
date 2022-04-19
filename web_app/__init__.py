a36da90369929fe1e05f041656b70cf71dca5971
eugeniecm <97541454+eugeniecm@users.noreply.github.com>
2022-04-19 15:51:49 -0400

    Flash Messaging

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,6 +1,4 @@
-
 # web_app/__init__.py
-
 
 import os
 from dotenv import load_dotenv
@@ -12,7 +10,7 @@ from web_app.routes.weather_routes import weather_routes
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", default="EUGENIE") # set this to something else
+SECRET_KEY = os.getenv("SECRET_KEY", default="super secret") # set this to something else on production!!!
 
 def create_app():
     app = Flask(__name__)
